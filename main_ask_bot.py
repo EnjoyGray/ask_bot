169905bf6fcb4a231fc9d2b42826feffa6c4d027
EnjoyGray <vanya.konrad@gmail.com>
2023-05-27 14:27:41 +0200

    fixup! - changing the name of the button

--- main_ask_bot.py
+++ main_ask_bot.py
@@ -19,7 +19,7 @@ async def bot_message(message: types.Message):
     if message.text == '❕Я ніколи не...❕':
         await bot.send_message(message.from_user.id, f'{qst_inv200()}', reply_markup=markups.mainMenu)
 
-    elif message.text == '❗️Рандомне питання❗️':
+    elif message.text == '❗️Випадкове питання❗️':
         await bot.send_message(message.chat.id, f"{qst500()}.❓", reply_markup=markups.mainMenu)
 
 
--- markups/markups.py
+++ markups/markups.py
@@ -3,6 +3,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # ---main menu---
 i_nvn = KeyboardButton('❕Я ніколи не...❕', row_width=1)
-qst_gen = KeyboardButton('❗️Рандомне питання❗️', row_width=1)
+qst_gen = KeyboardButton('❗️Випадкове питання❗️', row_width=1)
 
 mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(i_nvn, qst_gen)
