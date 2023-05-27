6ba300e67190b58448f5c24ed73c5e17f3e59861
EnjoyGray <vanya.konrad@gmail.com>
2023-05-27 16:06:00 +0200

    added a new section of questions "for a game of two - pairs" correction of small errors in the texts

--- database/qst_database.py
+++ database/qst_database.py
@@ -6,7 +6,7 @@ def qst_inv200():
     with open(r"database\I_never.json", "r", encoding='utf-8') as json_file:
         i_nvn = json.load(json_file)
 
-    num = str(randint(1, 200))
+    num = str(randint(1, 200 + 1))
     invn = i_nvn.get(num)
     return invn
 
@@ -15,11 +15,21 @@ def qst500():
     with open(r"database\qst.json", "r", encoding='utf-8') as js_file:
         qsr = json.load(js_file)
 
-    nm = str(randint(1, 500))
+    nm = str(randint(1, 500 + 1))
     qst = qsr.get(nm)
     return qst
+
+
+def qst_couple():
+    with open(r"database\couple.json", "r", encoding='utf-8') as j_file:
+        qsr = json.load(j_file)
+
+    nm = str(randint(1, 150 + 1))
+    cpl = qsr.get(nm)
+    return cpl
 
 
 if __name__ == "__main__":
     qst_inv200()
     qst500()
+    qst_couple()
--- main_ask_bot.py
+++ main_ask_bot.py
@@ -1,4 +1,4 @@
-from database.qst_database import qst_inv200, qst500
+from database.qst_database import qst_inv200, qst500, qst_couple
 from aiogram import Bot, Dispatcher, executor, types
 from markups import markups
 from markups.token import MyApiTOKEN
@@ -11,7 +11,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def commad_start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Бот підключенний!👋', reply_markup=markups.mainMenu)
+    await bot.send_message(message.from_user.id, '✅ Бот підключенний!👋', reply_markup=markups.mainMenu)
 
 
 @dp.message_handler()
@@ -22,6 +22,8 @@ async def bot_message(message: types.Message):
     elif message.text == '❗️Випадкове питання❗️':
         await bot.send_message(message.chat.id, f"{qst500()[:-1]}❓", reply_markup=markups.mainMenu)
 
+    elif message.text == '👫Для двох👫':
+        await bot.send_message(message.chat.id, f"{qst_couple()[:-1]}❓", reply_markup=markups.mainMenu)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
